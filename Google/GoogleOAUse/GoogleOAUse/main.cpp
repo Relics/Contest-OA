1759476a32aa37422fe7c962557515da786bec12
lucien <lcpingping@gmail.com>
2016-11-18 10:43:04 -0800

    OA contest
    leetcode work

---
+++ Google/GoogleOAUse/GoogleOAUse/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bitset>
+#include <ctime>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cctype> // For the isDigit
+#include <stack>
+#include <sstream> // For the change between int and string or any type
+#include <set>
+//int a=10;
+//stringstream ss;
+//ss<<a;
+//return ss.str();
+
+typedef long long ll;
+
+
+
+
+
+int main(){
+    
+    return 0;
+}
--- Google/googleOA/googleOA/main.cpp
+++ Google/googleOA/googleOA/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+string deletedash(string s) {
+    string res = "";
+    for (auto c : s) {
+        if (c != '-') {
+            if (c>='a' && c<='z')
+                res += toupper(c);
+            else
+                res += c;
+        }
+    }
+    return res;
+}
+
+string input(string s, int k) {
+    if (s == "" || k == 0) return "";
+    string temp = deletedash(s);
+    string ans = "";
+    int size = (int)temp.size();
+    int firstPart = size % k;
+    ans += temp.substr(0, firstPart);
+    for (int i=firstPart; i<=size-k; i+=k) {
+        if (i == firstPart && ans!= "") ans += '-';
+        if (i != size-k) ans += temp.substr(i, k) + '-';
+        else ans += temp.substr(i, k);
+    }
+    return ans;
+}
+
+
+int main() {
+    cout << input("aa-a-aAAA--123--Aaaabbb--aefaewffeBBB", 100) << endl;
     
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
